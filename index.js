b2a21a3ca4afbcf3600cea4ee4ae22cd440bf6a0
mvuoti <vuoti@iki.fi>
2019-09-28 16:36:23 +0300

    4.1 blogilista, step1; koodista npm-projekti+restclient

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const mongoPassword = process.env.MONGO_PASSWORD
+if (!mongoPassword) {
+	console.log('MONGO_PASSWORD?')
+	process.exit(1)
+}
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = `mongodb+srv://user:${mongoPassword}@cluster0-vsvmn.mongodb.net/blogapp?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
